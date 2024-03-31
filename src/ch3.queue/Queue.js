9d1fd5176b68070b190cf121f782d94dc09df8f3
Chansol Jeong <mutongzang@gmail.com>
2024-03-31 23:17:15 +0900

    queue 구현

---
+++ src/ch3.queue/Queue.js
@@ -0,0 +1,29 @@
+"use strict";
+class Queue {
+    items;
+    constructor() {
+        this.items = [];
+    }
+    enqueue(item) {
+        this.items.push(item);
+    }
+    dequeue() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items.shift();
+    }
+    front() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[0];
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    size() {
+        return this.items.length;
+    }
+}
+const q = new Queue();
---
+++ src/ch3.queue/Queue.ts
@@ -0,0 +1,36 @@
+class Queue<T> {
+
+    private items: T[];
+
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(item: T): void {
+        this.items.push(item)
+    }
+
+    dequeue(): T | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items.shift();
+    }
+
+    front(): T | undefined {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[0];
+    }
+
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+
+    size(): number {
+        return this.items.length;
+    }
+}
+
+const q = new Queue();

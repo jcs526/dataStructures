ffff69c241a5914868cf1fa4976644f41213efbb
Chansol Jeong <mutongzang@gmail.com>
2024-04-13 12:07:54 +0900

    LinkedList로 스택 만들기 위한 밑작업

---
+++ src/ch4.Linked_List/Stack.ts
@@ -0,0 +1,29 @@
+class StackWithLinkedList<T extends unknown> {
+    private data: T[] = [];
+    private top = -1;
+
+    constructor(private capacity = Infinity) {
+    }
+
+    push(...items: T[]): void {
+
+    }
+
+    pop(): T {
+
+    }
+
+    peek(): T {
+
+    }
+
+    isEmpty(): boolean {
+
+    }
+
+    size(): number {
+
+    }
+}
+
+
